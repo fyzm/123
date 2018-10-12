f72660130b347df1aa4f03da4c4333b7bac2f485
fyzm <fangyuan931018@gmail.com>
2018-10-12 21:57:32 +0800

    将代码进行MVC,V和C进行分离

--- resume/js/smooth-navigation.js
+++ resume/js/smooth-navigation.js
@@ -1,27 +1,24 @@
 !function () {
-  let liTags = document.querySelectorAll('nav.menu > ul >li')
-  for (let i = 0; i < liTags.length; i++) {
-    liTags[i].onmouseenter = function (x) {
-      x.currentTarget.classList.add('active')
-    }
-    liTags[i].onmouseleave = function (x) {
-      x.currentTarget.classList.remove('active')
-    }
-  }
-  let aTags = document.querySelectorAll('nav.menu > ul > li > a')
-  function animate(time) {
-    requestAnimationFrame(animate);
-    TWEEN.update(time);
-  }
-  requestAnimationFrame(animate);
-  for (let i = 0; i < aTags.length; i++) {
-    aTags[i].onclick = function (x) {
-      x.preventDefault()
-      let a = x.currentTarget
-      let href = a.getAttribute('href')
-      let element = document.querySelector(href)
-      let top = element.offsetTop
+  var view = document.querySelector('nav.menu')
+  view.style.border = '1px solid red'
 
+  var controller = {
+    view:null,
+    aTags:null,
+    init:function(view){
+      this.view = view
+      this.initAnimation()
+      this.bindEvents()
+    },
+    initAnimation:function(){
+      function animate(time) {
+        requestAnimationFrame(animate);
+        TWEEN.update(time);
+      }
+      requestAnimationFrame(animate);
+    },
+    scrollToElement:function(element){
+      let top = element.offsetTop
       let currentTop = window.scrollY
       let targetTop = top - 80
       let s = targetTop - currentTop // 路程
@@ -36,8 +33,22 @@
           window.scrollTo(0, coords.y) // 如何更新界面
         })
         .start(); // 开始缓动
-
-    }
+    },
+    bindEvents:function(){
+      let aTags = this.view.querySelectorAll('nav.menu > ul > li > a')
+      for (let i = 0; i < aTags.length; i++) {
+        aTags[i].onclick = function (x) {
+          x.preventDefault()
+          let a = x.currentTarget
+          let href = a.getAttribute('href')
+          let element = document.querySelector(href)
+         
+  
+        }
+      }
+    },
   }
+
+  controller.init(view)
 }.call()
 
